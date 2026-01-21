a5bf934775b565f197213d64696c73be9a2a5f0a
RidwanAlvi9999 <ridwanalvi9999@gmail.com>
2026-01-21 19:14:41 +0600

    Delete forgot.php

--- forgot.php
+++
@@ -1,7 +0,0 @@
-
-<h3>Forgot Password</h3>
-<?= isset($msg)?$msg:'' ?>
-<form method="post">
-<input name="email" type="email" required placeholder="Email"><br>
-<button>Send Token</button>
-</form>
