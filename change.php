f2ff64de929673c5bb9dff50e61b4468e7976ca7
RidwanAlvi9999 <ridwanalvi9999@gmail.com>
2026-01-21 19:15:10 +0600

    Delete change.php

--- change.php
+++
@@ -1,7 +0,0 @@
-
-<h3>Change Password</h3>
-<form method="post">
-<input name="token" required placeholder="Reset Token"><br>
-<input name="password" type="password" required placeholder="New Password"><br>
-<button>Change</button>
-</form>
